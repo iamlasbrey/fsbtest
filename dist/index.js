bff7174ed0b069a7d82e95b6d4fd3761dcda5561
Uzoma <iamlasbrey@gmail.com>
2023-08-31 15:00:27 +0100

    deployment

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const accountRoute = require("../routes/accounts");
+//routes
+app.use('/api/v1/F4BTEST', accountRoute);
+app.listen(3003, () => {
+    console.log('Listening on port 3003');
+});
+//# sourceMappingURL=index.js.map
